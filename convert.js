5f69091bf0c709c6ae16cde52d7abc3ce43a8752
henryccyu <henryccyu@gmail.com>
2019-07-17 23:38:37 -0700

    Update convert.js

--- convert.js
+++ convert.js
@@ -23,7 +23,52 @@ function convert()
     } while (index >= 0);
     
     result += inputText;
+    
+    result = ConvertBulletPoints(result);
+    result = ConvertNumberedList(result);
+    
     //console.log("result: " + result);
     
     document.getElementById("markup").value = result;
+}
+
+function getBibleGatewayLink()
+{
+    var inputText = document.getElementById("input").value;
+    result = "<a href=\"https://www.biblegateway.com/quicksearch/?quicksearch=" + inputText + "&qs_version=CUVMPT\">" + inputText + "</a>\r\n";
+    result += "[" + inputText + "](https://www.biblegateway.com/quicksearch/?quicksearch=" + inputText + "&qs_version=CUVMPT)";
+    document.getElementById("markup").value = result;
+}
+
+function ConvertBulletPoints(txt)
+{
+    var result = "";
+
+    lines = txt.split("\n");
+    for (i=0; i<lines.length; i++) {
+        index = lines[i].indexOf("+ ");
+        result += (index >= 0 ? "<li>" + lines[i].substring(index + 2, lines[i].length) + "</li>" : lines[i]) + "\r\n";
+        console.log("result: " + result);
+    }
+
+    return result;
+}
+
+function ConvertNumberedList(txt)
+{
+    var result = "";
+
+    do {
+        //console.log("txt: " + txt);
+        index = txt.search(/[0-9]\. /g);
+        if (index >= 0) {
+            result += txt.substring(0, index + 1) + "\uff0e";
+            //console.log("result: " + result);
+            txt = txt.substring(index + 3);
+        }
+    } while (index >= 0);
+    
+    result += txt;
+
+    return result;
 }
