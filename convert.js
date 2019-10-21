ee9879281358b48649f6dd414a55cdc1d1233f6e
Henry Yu <henryy@microsoft.com>
2019-10-20 21:28:46 -0700

    1) Move all the buttons to the top; 2) Allow stitching paragraphs in English (with a space in between when stitching English).

--- convert.js
+++ convert.js
@@ -39,7 +39,7 @@ function stitchParagraph()
             stitch = false;
             result += "\r\n";
         } else {
-            result += lines[i];
+            result += lines[i] + (document.getElementById("english").checked ? " " : "");
         }
 
 	    if (!stitch) {
